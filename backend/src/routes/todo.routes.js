5ecf58131367b0cc9117c806e79b0e72e5e2bf88
samsonremix17 <shambhavi.singh_cs23@gla.ac.in>
2026-04-03 14:47:15 +0530

    Added cookies and session

--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 const{getTodos, createTodo, deleteTodo, updateTodo} = require("../controllers/todo.controller");
+const {validateToDo} = require("../middleware/validateTodo.middleware");
+const auth = require("../middleware/auth.middleware");
 
 router.get("/",getTodos)
-router.post("/", createTodo);
+router.post("/", validateToDo, createTodo);
 router.put("/:id",updateTodo)
 router.delete("/:id", deleteTodo);
 
